a40061d7ed7ec834c888777c0e208975e51772ae
Kai Meyer <kai@gnukai.com>
2014-09-19 22:36:35 -0600

    Require CMake 3.0.2 and WIX

--- qeqecl_install.cmake
+++ qeqecl_install.cmake
@@ -15,17 +15,19 @@ if(WIN32)
 endif(WIN32)
 
 install(TARGETS ${PROJECT_NAME} 
-	BUNDLE DESTINATION . COMPONENT Runtime
-	RUNTIME DESTINATION bin COMPONENT Runtime
+	BUNDLE DESTINATION . #COMPONENT Runtime
+	RUNTIME DESTINATION bin #COMPONENT Runtime
 )
 
 #We aren't doing plugins
 #install(DIRECTORY "${QT_PLUGINS_DIR}/imageformats" DESTINATION ${plugin_dest_dir}/plugins COMPONENT Runtime)
 if(WIN32 OR APPLE)
 	# If we ever do Linux, we'll need to add \$ENV{DESTDIR}/ in there somewhere
-	INSTALL(CODE"
+	INSTALL(CODE "
 	file(WRITE \"\${CMAKE_INSTALL_PREFIX}/${qtconf_dest_dir}/qt.conf\" \"\")
-	" COMPONENT Runtime)
+	" 
+	#COMPONENT Runtime
+	)
 set(DIRS ${QT_LIBRARY_DIRS})
 
 install(CODE "
@@ -33,7 +35,9 @@ install(CODE "
       \"\${CMAKE_INSTALL_PREFIX}/${plugin_dest_dir}/plugins/*${CMAKE_SHARED_LIBRARY_SUFFIX}\")
     include(BundleUtilities)
     fixup_bundle(\"${APPS}\" \"\${QTPLUGINS}\" \"${DIRS}\")
-    " COMPONENT Runtime)
+    "
+    #COMPONENT Runtime
+    )
 endif()
 
 if(WIN32)
@@ -46,4 +50,7 @@ if(WIN32)
 	# set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY ${PROJECT_NAME})
 	set(CPACK_PACKAGE_INSTALL_DIRECTORY ${PROJECT_NAME})
 	set(CPACK_WIX_UPGRADE_GUID "7048B259-981C-49E7-A291-B89B67C7A20B")
+	set(CPACK_GENERATOR WIX)
+	set(CPACK_PACKAGE_EXECUTABLES ${PROJECT_NAME} "Kai's qEQEcl")
+	set(CPACK_WIX_PROGRAM_MENU_FOLDER "qEQEcl")
 endif()
